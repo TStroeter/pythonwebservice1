ea3380d45515ded71ae1c4177888c474a6d6d98e
Thomas Stroeter <stroeter@gmx.de>
2016-12-29 20:01:20 +0100

    Verzeichnisstruktur angepasst

--- testserver/cherrypitut01.py
+++ testserver/cherrypitut01.py
@@ -10,7 +10,7 @@ class HelloWorld(object):
     
 if __name__ == '__main__':
     cherrypy.config.update(
-        {'server.socket_port': 80,
+        {'server.socket_port': 8090,
         'engine.autoreload.on': False,
         'server.shutdown_timeout': 1 })    
     cherrypy.quickstart(HelloWorld())
